92a012a3525855b0f3034d93273e714a648ca4bd
luotao <luotaoyeah@gmail.com>
2024-08-19 20:33:52 +0800

    feat(doc): https://kotlinlang.org/docs/basic-syntax.html#package-definition-and-imports

---
+++ projects/doc/src/main/kotlin/com/luotao/learningkotlin/doc/basicsyntax/_01.kt
@@ -0,0 +1,11 @@
+// https://kotlinlang.org/docs/basic-syntax.html#package-definition-and-imports
+
+// 包的路径和文件路径无需完全对应, 跟 java 不一样,
+package com.luotao.learningkotlin.doc.basicsyntax._01
+
+// 程序的入口直接是 main 方法, 无需先定义类, 跟 java 不一样,
+fun main(args: Array<String>) {
+    println("HELLO")
+
+    println(99)
+}
