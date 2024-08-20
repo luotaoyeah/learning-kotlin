ea979ac6d2c9517297e513ce54b079de38db7b4a
luotao <luotaoyeah@gmail.com>
2024-08-20 19:51:45 +0800

    feat(doc): https://kotlinlang.org/docs/basic-syntax.html#for-loop

---
+++ projects/doc/src/main/kotlin/com/luotao/learningkotlin/doc/basicsyntax/_07.kt
@@ -0,0 +1,10 @@
+// https://kotlinlang.org/docs/basic-syntax.html#for-loop
+
+package com.luotao.learningkotlin.doc.basicsyntax._07
+
+
+fun main() {
+    for (i in listOf("A", "B", "C")) {
+        println(i)
+    }
+}
