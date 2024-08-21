b324ce1d96ad53aaf94d44cd71c4d3957bd9fb1f
luotao <luotaoyeah@gmail.com>
2024-08-21 19:38:34 +0800

    feat(doc): https://kotlinlang.org/docs/typecasts.html#is-and-is-operators

---
+++ projects/doc/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/types/typechecksandcasts/_01.kt
@@ -0,0 +1,18 @@
+// https://kotlinlang.org/docs/typecasts.html#is-and-is-operators
+
+package com.luotao.learningkotlin.doc.concepts.types.typechecksandcasts
+
+import org.junit.jupiter.api.Assertions
+import kotlin.test.Test
+
+class _01 {
+
+    // 使用 is 和 !is 判断对象是否是某个类型,
+    @Test
+    fun _01() {
+        Assertions.assertTrue(1 is Int)
+        Assertions.assertTrue(1 !is Long)
+
+        Assertions.assertTrue(1L is Long)
+    }
+}
