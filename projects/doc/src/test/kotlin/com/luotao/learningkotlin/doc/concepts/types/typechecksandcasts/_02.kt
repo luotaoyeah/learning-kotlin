7583b4856dd11ad0c619d8931cdc8c6158c47c3b
luotao <luotaoyeah@gmail.com>
2024-08-21 19:56:40 +0800

    feat(doc): https://kotlinlang.org/docs/typecasts.html#unsafe-cast-operator

---
+++ projects/doc/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/types/typechecksandcasts/_02.kt
@@ -0,0 +1,20 @@
+// https://kotlinlang.org/docs/typecasts.html#unsafe-cast-operator
+
+package com.luotao.learningkotlin.doc.concepts.types.typechecksandcasts
+
+import org.junit.jupiter.api.Assertions
+import kotlin.test.Test
+
+class _02 {
+
+    @Test
+    fun _01() {
+        // 使用 as 强制转换类型, 转换失败会抛异常,
+        Assertions.assertThrowsExactly(ClassCastException::class.java) {
+            var str01 = 0 as String
+        }
+
+        // 使用 as? 强制转换类型, 转换失败会返回 null,
+        Assertions.assertNull(0 as? String)
+    }
+}
