0ce7d4c6d28e47f76d795fc8f1f46075f7ccb02c
luotao <luotaoyeah@gmail.com>
2024-08-22 21:49:48 +0800

    feat(doc): https://kotlinlang.org/docs/properties.html#getters-and-setters

---
+++ projects/doc/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/classesandobjects/properties/_01.kt
@@ -0,0 +1,34 @@
+package com.luotao.learningkotlin.doc.concepts.classesandobjects.properties
+
+import org.junit.jupiter.api.Assertions
+import kotlin.test.Test
+
+// https://kotlinlang.org/docs/properties.html#getters-and-setters
+class _01 {
+
+    @Test
+    fun _01() {
+        class C01 {
+            // 有默认的 getter, setter,
+            var x: String = "x"
+
+            // 自定义的 getter, setter,
+            var y
+                get() = x.uppercase()
+                set(value) {
+                    x = value.lowercase()
+                }
+
+        }
+
+        var c01 = C01()
+
+        Assertions.assertEquals("x", c01.x)
+        Assertions.assertEquals("X", c01.y)
+
+
+        c01.y = "Y"
+        Assertions.assertEquals("y", c01.x)
+        Assertions.assertEquals("Y", c01.y)
+    }
+}
