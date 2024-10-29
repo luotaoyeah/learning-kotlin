2f7054801487cca507b4dee416393f668669658e
luotao <luotaoyeah@gmail.com>
2024-10-30 00:01:11 +0800

    feat(doc-2.0): https://kotlinlang.org/docs/properties.html#backing-fields

--- projects/doc-2.0/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/classesandobjects/properties/_03.kt
+++ projects/doc-2.0/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/classesandobjects/properties/_03.kt
@@ -8,12 +8,19 @@ class _03 {
     @Test
     fun _01() {
         class C01 {
-            // 不能直接声明字段, 但是可以在 getter, setter 中通过 field 访问该属性内部的字段,
+            // kotlin 中不能直接声明字段, 但是可以在 getter, setter 中通过 field 标识访问该 property 内部的字段,
             var x = 0
                 get() = field
                 set(value) {
                     field = value
                 }
+
+            // 什么时候会存在 backing field ?
+            //     1. 有默认的 getter 或/和 setter
+            //     2. 在自定义的 getter / setter 中访问了 field 标识,
+
+            // 因此如果只有自定义的 getter/setter, 并且在 getter/setter 中没有访问 field,
+            // 则该 property 就没有 backing field,
         }
     }
 }
