9a738313a92e867c92b8fbdb52bb2f8a15cb8299
luotao <luotaoyeah@gmail.com>
2024-11-14 20:16:48 +0800

    feat(doc-2.0): https://kotlinlang.org/docs/functions.html#named-arguments

--- projects/doc-2.0/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/functions/functions/_02.kt
+++ projects/doc-2.0/src/test/kotlin/com/luotao/learningkotlin/doc/concepts/functions/functions/_02.kt
@@ -1,25 +1,18 @@
 package com.luotao.learningkotlin.doc.concepts.functions.functions
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
 
-// https://kotlinlang.org/docs/functions.html#infix-notation
+// https://kotlinlang.org/docs/functions.html#named-arguments
 class _02 {
-
-    // 当一个方法满足下列条件时, 可以将它标记为 infix function:
-    //   1. 是一个类的方法, 或者扩展方法(extension function)
-    //   2. 只有一个参数
-    //   3. 参数不是 vararg, 参数没有默认值,
-
-    infix fun Int.myPlus(that: Int): Int {
-        return this + that
-    }
 
     @Test
     fun _01() {
-        Assertions.assertEquals(2, 1.myPlus(1))
 
-        // 可以使用下面这种形式来调用一个 infix function, 即省略 . 和 ()
-        Assertions.assertEquals(2, 1 myPlus 1)
+        fun fn01(a: String, b: Int, c: Boolean, d: Double) {
+            println("a: $a, b: $b, c: $c, d: $d")
+        }
+
+        // 调用函数的时候, 可以指定部分参数的名称,
+        fn01("A", c = false, b = 1, d = 0.01)
     }
 }
