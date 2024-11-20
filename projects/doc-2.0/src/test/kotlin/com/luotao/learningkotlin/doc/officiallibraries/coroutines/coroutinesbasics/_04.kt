d108de3d2534d286e070993e7a55e993ffd3b73c
luotao <luotaoyeah@gmail.com>
2024-11-20 23:27:01 +0800

    feat(doc-2.0): https://kotlinlang.org/docs/coroutines-basics.html#scope-builder-and-concurrency

--- projects/doc-2.0/src/test/kotlin/com/luotao/learningkotlin/doc/officiallibraries/coroutines/coroutinesbasics/_04.kt
+++ projects/doc-2.0/src/test/kotlin/com/luotao/learningkotlin/doc/officiallibraries/coroutines/coroutinesbasics/_04.kt
@@ -1,34 +1,35 @@
 package com.luotao.learningkotlin.doc.officiallibraries.coroutines.coroutinesbasics
 
 import com.luotao.learningkotlin.util.now
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 
-// https://kotlinlang.org/docs/coroutines-basics.html#scope-builder
-class _03 {
+// https://kotlinlang.org/docs/coroutines-basics.html#scope-builder-and-concurrency
+class _04 {
 
     @Test
     fun _01() {
-        println("${now()} | BEGIN")
 
-        /** [coroutineScope] 函数本身是一个 suspend 函数, 用于创建一个 [CoroutineScope], 并在其中执行指定的代码, */
+        /** [coroutineScope] 函数里面可以使用 [launch] 启动多个 coroutine, */
         suspend fun doWorld() {
             coroutineScope {
                 launch {
                     delay(2000)
-                    println("${now()} | WORLD")
+                    println("${now()} | CCC")
                 }
 
-                println("${now()} | HELLO")
+                launch {
+                    delay(1000)
+                    println("${now()} | BBB")
+                }
+
+                println("${now()} | AAA")
             }
         }
 
         runBlocking { doWorld() }
-
-        println("${now()} | END")
     }
 }
