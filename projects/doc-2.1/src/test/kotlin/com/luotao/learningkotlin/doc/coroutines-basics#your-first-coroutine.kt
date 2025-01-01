23c895a6370853627b0cca57e87667b5da323f5e
luotao <luotaoyeah@gmail.com>
2025-01-01 12:06:29 +0800

    feat(doc-2.1): https://kotlinlang.org/docs/coroutines-basics.html#structured-concurrency

--- projects/doc-2.1/src/test/kotlin/com/luotao/learningkotlin/doc/coroutines-basics#your-first-coroutine.kt
+++ projects/doc-2.1/src/test/kotlin/com/luotao/learningkotlin/doc/coroutines-basics#your-first-coroutine.kt
@@ -1,6 +1,7 @@
 package com.luotao.learningkotlin.doc
 
 import com.luotao.learningkotlin.util.log
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
@@ -9,23 +10,47 @@ import kotlin.test.Test
 class `coroutines-basics#your-first-coroutine` {
     @Test
     fun _01() {
-        log("AAA")
+        log("A")
 
         /**
          * [runBlocking] 是一个 coroutine builder, 用来创建一个 coroutine, 并且会阻塞当前线程,
          * 通常用来在非 coroutine 环境下开启 coroutine 环境, 比如 main 方法中, 或者单元测试中,
          */
         runBlocking {
-            log("CCC")
+            log("C")
 
             /**
              * [launch] 也是一个 coroutine builder, 用来创建一个 coroutine, 但是不会阻塞当前线程,
              */
             launch {
-                log("DDD")
+                log("D")
             }
+
+            log("E")
         }
 
-        log("BBB")
+        log("B")
+    }
+
+    // https://kotlinlang.org/docs/coroutines-basics.html#structured-concurrency
+    @Test
+    fun _02(){
+        log("A")
+
+        runBlocking {
+            log("C")
+            delay(1000)
+            log("D")
+
+            launch {
+                log("E")
+                delay(1000)
+                log("F")
+            }
+
+            log("G")
+        }
+
+        log("B")
     }
 }
