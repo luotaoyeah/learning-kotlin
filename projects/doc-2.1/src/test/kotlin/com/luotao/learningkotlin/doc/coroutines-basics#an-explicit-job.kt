d7ddcae7ef7fdc1c163692fabaaa15bb1ca5c8b0
luotao <luotaoyeah@gmail.com>
2025-01-01 12:57:52 +0800

    feat(doc-2.1): https://kotlinlang.org/docs/coroutines-basics.html#an-explicit-job

---
+++ projects/doc-2.1/src/test/kotlin/com/luotao/learningkotlin/doc/coroutines-basics#an-explicit-job.kt
@@ -0,0 +1,34 @@
+package com.luotao.learningkotlin.doc
+
+import com.luotao.learningkotlin.util.log
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+
+// https://kotlinlang.org/docs/coroutines-basics.html#an-explicit-job
+class `coroutines-basics#an-explicit-job` {
+    @Test
+    fun _01() {
+        runBlocking {
+            log("A")
+
+            /**
+             * [launch] 返回一个 [kotlinx.coroutines.Job] 对象,
+             */
+            val job = launch {
+                log("C")
+                delay(1000)
+                log("D")
+            }
+
+            log("B")
+
+            /**
+             * [kotlinx.coroutines.Job.join] 方法是一个 suspend function, 它会等待这个 [kotlinx.coroutines.Job] 完成, 然后再执行后面的代码,
+             */
+            job.join()
+            log("E")
+        }
+    }
+}
