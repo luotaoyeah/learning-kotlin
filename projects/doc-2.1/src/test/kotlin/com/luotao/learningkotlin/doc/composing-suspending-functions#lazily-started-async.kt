5e6e583750ac243b049cd905e6ecf6458bcb1370
luotao <luotaoyeah@gmail.com>
2025-01-01 16:18:38 +0800

    feat(doc-2.1): https://kotlinlang.org/docs/composing-suspending-functions.html#lazily-started-async

---
+++ projects/doc-2.1/src/test/kotlin/com/luotao/learningkotlin/doc/composing-suspending-functions#lazily-started-async.kt
@@ -0,0 +1,42 @@
+package com.luotao.learningkotlin.doc
+
+import com.luotao.learningkotlin.util.log
+import kotlinx.coroutines.*
+import kotlin.test.Test
+
+// https://kotlinlang.org/docs/composing-suspending-functions.html#lazily-started-async
+class `composing-suspending-functions#lazily-started-async` {
+    @Test
+    fun _01() {
+        suspend fun fnC() {
+            delay(1000)
+            log("C")
+        }
+
+        suspend fun fnD() {
+            delay(1000)
+            log("D")
+        }
+
+        runBlocking {
+            log("A")
+
+            /**
+             * 如果 [launch] 的 start 参数传 [CoroutineStart.LAZY], 则这个 coroutine 创建之后不会自己启动,
+             * 需要手动调用 [Job.join] 方法或者 [Job.start] 方法来启动它, 或者调用 [Deferred.await] 方法来启动它,
+             */
+            val jobC = launch(start = CoroutineStart.LAZY) { fnC() }
+            val jobD = launch(start = CoroutineStart.LAZY) { fnD() }
+
+            log("B")
+
+            delay(1000)
+            log("START C")
+            jobC.start()
+
+            delay(1000)
+            log("START D")
+            jobD.start()
+        }
+    }
+}
