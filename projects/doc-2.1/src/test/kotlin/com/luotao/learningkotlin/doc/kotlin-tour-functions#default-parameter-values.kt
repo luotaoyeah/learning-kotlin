a1e986d7077db9a552ad89c2d348b0ec44ac7a32
luotao <luotaoyeah@gmail.com>
2025-02-23 14:32:45 +0800

    feat(doc-2.1): https://kotlinlang.org/docs/kotlin-tour-functions.html#default-parameter-values

---
+++ projects/doc-2.1/src/test/kotlin/com/luotao/learningkotlin/doc/kotlin-tour-functions#default-parameter-values.kt
@@ -0,0 +1,15 @@
+package com.luotao.learningkotlin.doc
+
+import kotlin.test.Test
+
+// https://kotlinlang.org/docs/kotlin-tour-functions.html#default-parameter-values
+class `kotlin-tour-functions#default-parameter-values` {
+    @Test
+    fun _01() {
+        // 使用 = 定义参数的默认值, 「有默认值的参数」可以放到「无默认值的参数」前面,
+        fun fn01(x: Int = 1, y: String = "Y", z: Boolean) {}
+
+        // 调用时, 有默认值的参数可以不传,
+        fn01(1, z = false)
+    }
+}
